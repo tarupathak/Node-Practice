4bf16bffbbc26422a299107cdc1240974d788c07
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-04-17 21:45:20 +0530

    Applied nodemon package

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const colors= require('colors');
-console.log('package.json'.red)
+// const colors= require('colors');
+// console.log('package.json'.red);
+console.warn('Try taru');
+//node js is async and single threaded
