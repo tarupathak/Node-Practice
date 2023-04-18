ca602937d7e404641ab49db9988b10f7336c4eee
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-04-18 21:50:44 +0530

    Made my first simple api

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:'Taru Pathak',email:'pathaktaru@gmail.com',contact:'9999'},
+    {name:'Shreyansh Agarwal',email:'shreyansh@gmail.com',contact:'9999'},
+    {name:'Divyathak',email:'pathakdivya@gmail.com',contact:'9999'}
+];
+module.exports=data;
--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // const colors= require('colors');
 // console.log('package.json'.red);
-console.warn('Try taru');
-//node js is async and single threaded
+// console.warn('Try taru');
+// node js is async and single threaded
+
+const http = require('http');
+const data= require('./data');
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{'Content-Type':'application\json'});
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(5000);
