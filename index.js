0d6b9da1234eb14236dc4f8dbc3ba1c22a1b13b1
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-08 08:52:20 +0530

    learnt getting input from cammand line

--- index.js
+++ index.js
@@ -3,10 +3,26 @@
 // console.warn('Try taru');
 // node js is async and single threaded
 
-const http = require('http');
-const data= require('./data');
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{'Content-Type':'application\json'});
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(5000);
+// const http = require('http');
+// const data= require('./data');
+// http.createServer((req,resp)=>{
+//     resp.writeHead(200,{'Content-Type':'application\json'});
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+// }).listen(5000);
+
+// console.log(process.argv)
+
+// const fs = require("fs");
+// const input = process.argv;
+// fs.writeFileSync(input[2], input[3]);
+
+// if (input[2] == "add") {
+//   fs.writeFileSync(input[3], input[4]);
+// } else if (input[2] == "remove") {
+//   fs.unlinkSync(input[3]);
+// } else {
+//   console.log("Invalid input");
+// }
+
+// console.log(process.argv[2])
