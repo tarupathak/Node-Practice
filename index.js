e09b3f05067288720131213bb26f970c20026b2f
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-12 01:22:22 +0530

    learned removing extensions from URL and 404 page

--- index.js
+++ index.js
@@ -124,6 +124,16 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(publicPath)) 
+// app.use(express.static(publicPath)) 
+app.get('/',(req,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+})
 
+app.get('/about',(req,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('*',(req,resp)=>{
+    resp.sendFile(`${publicPath}/nopage.html`)
+})
 app.listen(5000);
