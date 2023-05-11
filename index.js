a76920b4048c195526de8b350174e856dae9ee2f
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-12 00:14:11 +0530

    Setup express js enviornment

--- index.js
+++ index.js
@@ -95,3 +95,21 @@
 
 // console.log("Finishing up")
 
+//<-----------EXPRESS JS ------------>
+
+const express = require("express");
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("Hello, this is Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello, this is About Page");
+});
+
+app.get("/help", (req, res) => {
+  res.send("Hello, this is Help Page");
+});
+
+app.listen(5000);
