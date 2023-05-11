8fc35f6dcda49227201bd83ee46ac60724203dff
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-12 00:24:10 +0530

    Got basic understating of request and response

--- index.js
+++ index.js
@@ -101,7 +101,8 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello, this is Home Page");
+  console.log("data sent by browser = ", req.query.name);
+  res.send("Welcome,"+ req.query.name);
 });
 
 app.get("/about", (req, res) => {
